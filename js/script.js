590e4df7fe9c1acfcbdf22223977c4a141a4d47e
Marc Moureau <113120354+C00MAR@users.noreply.github.com>
2022-11-23 10:02:26 +0100

    👽️ FINAL 👽️

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 $(".picto").on("click", function(){
     $("header").toggleClass("close");
     $(".picto").toggleClass("close-btn");
     $(".nav").toggleClass("open-menu");
 });
-=======
-$(".picto").on("click", function(){
-    $("header").toggleClass("close");
-    $(".picto").toggleClass("close-btn");
-    $(".nav").toggleClass("open-menu");
-});
->>>>>>> main
